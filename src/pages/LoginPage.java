bc1a060e558a88401eb65e533e3963f15becd94c
alokbaral09 <alokbaral09@gmail.com>
2025-12-10 23:47:02 +0530

    added,updated,deleted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -3,5 +3,10 @@ package pages;
 public class LoginPage {
 	public static void main(String []args) {
 		System.out.println("Hello LP");
+		
+		String un="admin";
+		String pwd="admin";
+		System.out.println("Logged in....");
+		
 	}
 }
