d663d374c4fc5f18b3d24ed6aeef0364b16fd791
alokbaral09 <55913903+alokbaral09@users.noreply.github.com>
2025-12-17 23:15:36 +0530

    Create ClassicPage.java

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class ClassicPage {
+
+	public  void classic() {
+		int k=10;
+		int k1=20;
+		int div=k1/k;
+		System.out.println(div);
+    System.out.println("Bye");
+
+	}
+
+}
